d25e2853b4b0113f6f07bf28d36a958f2b7691a7
okapi <jf@geonef.fr>
2014-08-27 01:32:23 +0200

    sysconf for http nodejs

---
+++ install.sh
@@ -0,0 +1,17 @@
+# sysconf install script for sysconf profile for this custom system
+
+. /usr/lib/sysconf.base/common.sh
+
+sysconf_require_packages nodejs \
+    || nef_fatal "could not install nodejs"
+
+sh install.npm.js \
+    || nef_fatal "could not install npm"
+
+npm install -g forever \
+    || nef_fatal "could not install npm module: forever"
+
+update-rc.d glxc-http-server defaults
+
+service glxc-http start \
+    || nef_fatal "could not start glxc-http"
