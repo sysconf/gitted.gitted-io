76f7510f7f2f3c5ed157a0487bea2be91d49a357
okapi <jf@geonef.fr>
2014-08-27 21:07:11 +0200

    sysconf/actual: fixed install

--- install.sh
+++ install.sh
@@ -14,29 +14,37 @@ if [ ! -x /usr/bin/npm ]; then
         || nef_fatal "could not install npm"
 fi
 
+###
+# START doing things in share/textree
+
+_old_pwd=$PWD
+cd tree/usr/share/textree
+
 # 'forever' come from NPM and provide NodeJS process management
 if ! npm list forever -g >/dev/null; then
     nef_log "Installing NPM modules..."
     npm install forever -g
-    _old_pwd=$PWD
-    cd tree/usr/share/textree
     npm install --unsafe-perm \
         || nef_fatal "could not install npm modules for textree"
-    # npm install --unsafe-perm --verbose http://registry.npmjs.org/nodegit/-/nodegit-0.1.4.tgz \
-    #     || nef_fatal "could not build and install nodegit-0.1.4"
-    nef_log "Building and installing nodegit (which is really tricky)..."
-    (
-        cd node_modules \
-            && mkdir nodegit \
-            && curl http://registry.npmjs.org/nodegit/-/nodegit-0.1.4.tgz \
-            | tar xz --strip-components=1 -C ./nodegit \
-            && cd nodegit \
-            && npm --unsafe-perm install ejs \
-            && npm --unsafe-perm run codegen \
-            && npm --unsafe-perm install
-    ) || nef_log "failed to build and install nodegit"
-    cd $_old_pwd
 fi
+npm install --unsafe-perm --verbose http://registry.npmjs.org/nodegit/-/nodegit-0.1.4.tgz \
+    || nef_fatal "could not build and install nodegit-0.1.4"
+# nef_log "Building and installing nodegit (which is really tricky)..."
+# (
+#     cd node_modules \
+#         && mkdir nodegit \
+#         && curl http://registry.npmjs.org/nodegit/-/nodegit-0.1.4.tgz \
+#         | tar xz --strip-components=1 -C ./nodegit \
+#         && cd nodegit \
+#         && npm --unsafe-perm install ejs \
+#         && npm --unsafe-perm run codegen \
+#         && npm --unsafe-perm install
+# ) || nef_log "failed to build and install nodegit"
+
+cd $_old_pwd
+
+# END doing things in share/textree
+###
 
 # Manage the textree service
 if [ 0 -eq $(find /etc/rc*.d -name '*[0-9]textree' | wc -l) ]; then
